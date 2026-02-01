6db169bcb41eaa7ec32c19de3f0b225b9b711861
Gizmotronn <liam@skinetics.tech>
2026-02-01 12:40:21 +0800

    🥝🪹 ↝ [#5]: Fixing js tests

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = {
   plugins: [["react-native-worklets-core/plugin"]],
   env: {
     test: {
-      presets: ["module:metro-react-native-babel-preset"],
+      presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+      ],
     },
   },
 };
